a453b21be03363eb277e230e476fbd3f42858255
Mahliyo <gasandigital13@gmail.com>
2026-03-29 15:54:38 +0500

    main.js input

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 function addTodo (){
     const input = document.getElementById("input");
     const btn = document.getElementById("btn");
-    const btn = document.getElementById("btn");
-
+    const list = document.getElementById("list");
+  if (input.value === "") return;
+  let  item = document.createElement("li")
 } 
