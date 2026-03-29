00c51d6588917f127e101c0e977aa5d3a3719565
Mahliyo <gasandigital13@gmail.com>
2026-03-29 15:50:30 +0500

    main.js input

--- main.js
+++ main.js
@@ -2,5 +2,6 @@
 function addTodo (){
     const input = document.getElementById("input");
     const btn = document.getElementById("btn");
-    
+    const btn = document.getElementById("btn");
+
 } 
