4ca50f8d9baf2e8f52b16b2ea1ed00d99fb40974
Mahliyo <gasandigital13@gmail.com>
2026-03-29 16:01:48 +0500

    main.js btn

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 
 function addTodo (){
     const input = document.getElementById("input");
-    const btn = document.getElementById("btn");
     const list = document.getElementById("list");
   if (input.value === "") return
   let  item = document.createElement("item");
