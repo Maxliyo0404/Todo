eb3f1b4eb044712d57bf44d5ade9b783294f38ce
Mahliyo <gasandigital13@gmail.com>
2026-03-29 16:34:24 +0500

    main.js list

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ function addTodo (){
   if (input.value === "") return
   let  li = document.createElement("li");
   li.textContent = input.value;
+  li.innerHTML  = <span>${input.value}</span>
+ <button class ="delete-btn"</button>
   list.appendChild("li");
   input.value = "";
 } 
